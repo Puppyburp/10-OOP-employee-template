8f9fe615421afbe1792db1a036b11834b258784e
Jennifer Walason <jenniferwalason@gmail.com>
2020-07-05 14:37:37 -0400

    primary files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// dependencies
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,31 +6,117 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+// questions for all
+const allQuestions = [{
+  type: "input",
+  message: "Team member name:",
+  name: "name",
+},
+{
+  type: "number",
+  message: "Team member id:",
+  name: "id",
+},
+{
+  type: "input",
+  message: "Email address:",
+  name: "email",
+}];
+// manager specific question
+const mgrQuestions = [{
+  type: "input",
+  message: "Manager's office number:",
+  name: "officeNumber",
+}];
+// engineer specific question
+const engQuestions = [{
+  type: "input",
+  message: "Engineer's GitHub name:",
+  name: "github",
+}];
+// intern specific question
+const intQuestions = [{
+  type: "input",
+  message: "Intern's school:",
+  name: "school",
+}];
 
+let allQ = [];
+const manager = [];
+const engineer = [];
+const intern = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function getRole() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "What type of team member would you like to add?",
+            name: "position",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "Finished"
+            ],
+        }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    ).then(function (data) {
+        console.log(data.position) 
+        if (data.position === "Finished") {
+            renderHTML();
+        }
+        else if (data.position === "Manager") {
+            allQ = [...allQuestions, ...mgrQuestions];
+            getData(data.position);
+        }
+        else if (data.position === "Engineer") {
+            allQ = [...allQuestions, ...engQuestions];
+            getData(data.position);
+        }
+        else if (data.position === "Intern") {
+            allQ = [...allQuestions, ...intQuestions];
+            getData(data.position);
+        }
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function getData(position) {
+    inquirer.prompt(allQ)
+        .then(function (data) {
+        const {name, id, email, officeNumber, github, school} = data;
+        switch (position) {
+            case "Manager":
+                manager.push(new Manager(name, id, email, officeNumber));
+                break;
+            case "Engineer":
+                engineer.push(new Engineer(name, id, email, github));
+                break;
+            case "Intern":
+                intern.push(new Intern(name, id, email, school));
+                break;
+        }
+        console.log(manager);
+        console.log(engineer);
+        console.log(intern);
+        getRole();
+    });
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+}
+async function renderHTML(){
+  try {
+      const employees = [...manager, ...engineer, ...intern];
+      const pageContent = await render(employees);
+      fs.writeFile("output/employee-summary.html", pageContent, err => {
+          if (err){
+              return console.log(err);
+          }
+          console.log("Success! Your file has been created!");
+      });
+  }
+  catch (err) {
+      console.log(err);
+  }
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+getRole();
